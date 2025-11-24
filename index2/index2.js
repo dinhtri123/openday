d74186a51e9338c4820f7a20f8cac36b1f9e694f
dinhtri123 <phamdinhtriqs111@gmail.com>
2025-11-24 15:37:59 +0700

    page overview

--- index2/index2.js
+++ index2/index2.js
@@ -13,22 +13,56 @@ document.addEventListener("DOMContentLoaded", () => {
   const counters = document.querySelectorAll(".counter");
   counters.forEach((counter) => {
     const target = parseInt(counter.dataset.target);
-    const duration = 2000; 
+    const duration = 2000;
     const interval = 16;
     const steps = duration / interval;
     const increment = target / steps;
     let current = 0;
-    
-    if(target > current) {
+
+    if (target > current) {
       const counterInterval = setInterval(() => {
         current += increment;
         counter.textContent = Math.floor(current);
-        
-        if(current >= target) {
+
+        if (current >= target) {
           clearInterval(counterInterval);
           counter.textContent = target;
         }
       }, interval);
     }
   });
-}); 
+  // tab
+  const tabItems = document.querySelectorAll(".overview-train-tab-item");
+  const trainContents = document.querySelectorAll(".train-content-tab");
+  tabItems.forEach((tabItem) => {
+    tabItem.addEventListener("click", () => {
+      trainContents.forEach((trainContent) => {
+        trainContent.classList.remove("active");
+      });
+      tabItems.forEach((item) => {
+        item.classList.remove("active");
+      });
+      tabItem.classList.add("active");
+      const dataTab = tabItem.dataset.tab;
+      const trainContent = document.getElementById(dataTab);
+      trainContent.classList.add("active");
+    });
+  });
+
+  // tab major mobile
+  const media = window.matchMedia("(max-width: 767px)");
+  if (media.matches) {
+    const tabTitleMajor = document.querySelectorAll(
+      ".overview-major-item-title"
+    );
+    const tabMajorItem = document.querySelectorAll(".overview-major-item");
+    tabTitleMajor.forEach((tabTitle) => {
+      tabTitle.addEventListener("click", () => {
+        tabMajorItem.forEach((item) => {
+          item.classList.remove("active");
+        });
+        tabTitle.parentElement.classList.add("active");
+      });
+    });
+  }
+});
