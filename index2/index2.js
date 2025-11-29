607151455000db9391411c7f48b62543e92ddb8a
dinhtri123 <phamdinhtriqs111@gmail.com>
2025-11-29 12:36:29 +0700

    fix update 29/11

--- index2/index2.js
+++ index2/index2.js
@@ -2,19 +2,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const media = window.matchMedia("(max-width: 767px)");
   const iconMenu = document.querySelector(".icon-menu");
   const iconCloseMenu = document.querySelector(".icon-close-menu");
-  const headerNav = document.querySelector(".header-nav");
-  const headerMenu = document.querySelector(".header-menu");
-  const headerTop = document.querySelector(".header-top");
+  const popupMenu = document.querySelector(".popup-menu");
   const iconSearchSm = document.querySelector(".icon-search-sm");
-  const headerSearchSm = document.querySelector(".header-search.sm .header-top-search");
+  const headerSearchSm = document.querySelector(
+    ".header-search.sm .header-top-search"
+  );
   iconMenu.addEventListener("click", () => {
-    headerNav.classList.toggle("active");
+    popupMenu.classList.toggle("active");
   });
   iconCloseMenu.addEventListener("click", () => {
-    headerNav.classList.remove("active");
+    popupMenu.classList.remove("active");
   });
-  if(media.matches) {
-    headerMenu.appendChild(headerTop);
+  if (media.matches) {
     iconSearchSm.addEventListener("click", () => {
       headerSearchSm.classList.toggle("active");
     });
@@ -45,20 +44,22 @@ document.addEventListener("DOMContentLoaded", () => {
   // tab
   const tabItems = document.querySelectorAll(".overview-train-tab-item");
   const trainContents = document.querySelectorAll(".train-content-tab");
-  tabItems.forEach((tabItem) => {
-    tabItem.addEventListener("click", () => {
-      trainContents.forEach((trainContent) => {
-        trainContent.classList.remove("active");
+  if(tabItems.length > 0) {
+    tabItems.forEach((tabItem) => {
+      tabItem.addEventListener("click", () => {
+        trainContents.forEach((trainContent) => {
+          trainContent.classList.remove("active");
+        });
+        tabItems.forEach((item) => {
+          item.classList.remove("active");
+        });
+        tabItem.classList.add("active");
+        const dataTab = tabItem.dataset.tab;
+        const trainContent = document.getElementById(dataTab);
+        trainContent.classList.add("active");
       });
-      tabItems.forEach((item) => {
-        item.classList.remove("active");
-      });
-      tabItem.classList.add("active");
-      const dataTab = tabItem.dataset.tab;
-      const trainContent = document.getElementById(dataTab);
-      trainContent.classList.add("active");
     });
-  });
+  }
 
   // tab major mobile
   if (media.matches) {
@@ -105,7 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (campusItems.length > 0) {
     activateItem(0);
-    if(!media.matches) {
+    if (!media.matches) {
       startAutoRotate();
     }
   }
@@ -123,12 +124,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // show more major
   const btnShowMoreMajor = document.querySelector(".overview-major-btn");
-  btnShowMoreMajor.addEventListener("click", (e) => {
-    const majorList = document.querySelector(".overview-major-list");
-    majorList.classList.toggle("active");
-    majorList.style.maxHeight = majorList.classList.contains("active") ? `${majorList.scrollHeight}px` : "300px";
-    btnShowMoreMajor.querySelector("span").textContent = majorList.classList.contains("active") ? "Thu gọn" : "Xem thêm";
-  });
+  if(btnShowMoreMajor) {
+    btnShowMoreMajor.addEventListener("click", (e) => {
+      const majorList = document.querySelector(".overview-major-list");
+      majorList.classList.toggle("active");
+      majorList.style.maxHeight = majorList.classList.contains("active")
+        ? `${majorList.scrollHeight}px`
+        : "300px";
+      btnShowMoreMajor.querySelector("span").textContent =
+        majorList.classList.contains("active") ? "Thu gọn" : "Xem thêm";
+    });
+  }
 
-  
+  // life desktop hover 
+  if(!media.matches) {
+    const lifeDkItems = document.querySelectorAll(".life-dk-item");
+    lifeDkItems.forEach((lifeDkItem) => {
+      lifeDkItem.addEventListener("mouseenter", () => {
+        lifeDkItems.forEach((item) => {
+          item.classList.remove("active");
+        });
+        lifeDkItem.classList.add("active");
+      });
+    });
+    lifeDkItems.forEach((lifeDkItem) => {
+      lifeDkItem.addEventListener("mouseleave", () => {
+        lifeDkItems.forEach((item) => {
+          item.classList.remove("active");
+        });
+      });
+    });
+  }
 });
